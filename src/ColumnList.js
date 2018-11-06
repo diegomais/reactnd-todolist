3661ce83cab465f8998137b907e3edb23a200e58
diegomais <diegomais@live.com>
2018-11-06 18:25:38 -0200

    Solution code

---
+++ src/ColumnList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import If from './If.js'
+import './ColumnList.css'
+
+const ColumnList = ({ tasks, columnTitle, updateTask, addTask }) => {
+  const currentTasks = tasks.filter(task => task.status === columnTitle);
+  return (
+    <div className="column-list">
+      <h3>{columnTitle}</h3>
+
+      <If test={columnTitle === 'To Do'}>
+        <form onSubmit={addTask}>
+          <input type="text" />
+          <button type="submit">Criar tarefa</button>
+        </form>
+      </If>
+
+      <ul>
+        {currentTasks.map(task => (
+          <li key={task.id}>
+            <input
+              type="checkbox"
+              onChange={e => updateTask(e.target, task)}
+              checked={columnTitle === 'Done'}
+            />
+            <span>{task.description}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ColumnList
